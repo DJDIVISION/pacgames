4052c0633a15e65f1a5e52c99547691bf2d9036d
Victor <bodegaflamenca666@gmail.com>
2024-08-30 19:35:31 +0200

    third

--- server/index.jsx
+++ server/index.jsx
@@ -6,7 +6,9 @@ const PORT = 8000; // Or any port you like
 
 app.use(cors());
 
-
+app.use("/", (req, res) => {
+  res.send("Server is running");
+})
 
 app.listen(PORT, () => {
   console.log(`Proxy server running on http://localhost:${PORT}`);
