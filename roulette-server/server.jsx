b9aa72a4597bbb0915b9a3e7562d907a9880f2e8
Victor <bodegaflamenca666@gmail.com>
2024-10-29 18:26:26 +0100

    newHome8

--- roulette-server/server.jsx
+++ roulette-server/server.jsx
@@ -51,7 +51,7 @@ const americanRouletteNumbers = [
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://pacgames-frontend.onrender.com",
   methods: ['GET', 'POST'],
   credentials: true
 }));
@@ -61,7 +61,7 @@ app.get('/', (req, res) => {
 const httpServer = createServer(app);
 const io = new Server(httpServer, { 
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://pacgames-frontend.onrender.com",
     methods: ["GET", "POST"]  // Client URL
   },
  });
