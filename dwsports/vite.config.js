273714bb20cccef268c937c1d0d4d39ae110f6c5
Victor <bodegaflamenca666@gmail.com>
2024-11-19 10:41:08 +0100

    champions10

--- dwsports/vite.config.js
+++ dwsports/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa';
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import svgr from 'vite-plugin-svgr';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,18 +11,15 @@ export default defineConfig({
     react(),
     nodePolyfills(),
     svgr(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          version: '1.0.1', // Add your app version here
+        },
+      },
+    }),
   ],
   optimizeDeps: {
     include: ['@tonconnect/ui-react']
-  },
-  build: {
-    assetsInlineLimit: 0, // Optional, ensures assets aren't inlined
-    rollupOptions: {
-      output: {
-        entryFileNames: '[name].[hash].js',
-        chunkFileNames: '[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]',
-      },
-    },
-  },
+  }
 })
