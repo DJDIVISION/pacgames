6adc3d21eb12925ed07e67889f986e2b44665f0a
Victor <bodegaflamenca666@gmail.com>
2024-12-11 08:58:37 +0100

    CHAMPIONS5

--- dwsports/src/pages/NewRoulette.jsx
+++ dwsports/src/pages/NewRoulette.jsx
@@ -75,7 +75,7 @@ import { message as ANTDmessage } from 'antd';
 import { ArrowLeftRelative, StyledButton } from './index.jsx';
 import RouletteTabs from '../components/roulette/RouletteTabs.jsx';
 
-const socket = io.connect("https://pactongamingzone.vercel.app")
+/* const socket = io.connect("https://pactongamingzone.vercel.app") */
 
 const Roulete = () => {
 
